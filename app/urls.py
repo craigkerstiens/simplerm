409a331511eb4f9bd6db1252c51fc075b194a6c9
Craig Kerstiens <craig.kerstiens@gmail.com>
2012-01-16 20:55:39 -0800

    making invite page visible

--- app/urls.py
+++ app/urls.py
@@ -5,10 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'', include('privatebeta.urls')),
     url(r'', include('app.rm.urls')),    
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
     url(r'', include('social_auth.urls')),
-    url(r'^invites/', include('privatebeta.urls')),
 )

4eb0c766427f0a0f418387137081d4d2364d036d
Craig Kerstiens <craig.kerstiens@gmail.com>
2012-02-26 11:55:49 -0800

    updating to use grappelli

--- app/urls.py
+++ app/urls.py
@@ -9,9 +9,12 @@ nexus.autodiscover()
 urlpatterns = patterns('',
     url(r'', include('privatebeta.urls')),
     url(r'', include('app.rm.urls')),    
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^nexus/', include(nexus.site.urls)),
+    url(r'^impersonate/', include('impersonate.urls')),
     url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
     url(r'', include('social_auth.urls')),
 )

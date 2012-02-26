876719fe08786cbedc9ba4f1dd00ff71230ef06d
Craig Kerstiens <craig.kerstiens@gmail.com>
2012-02-26 15:11:22 -0800

    testing

--- app/urls.py
+++ app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^nexus/', include(nexus.site.urls)),
     url(r'^impersonate/', include('impersonate.urls')),
-    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
     url(r'', include('social_auth.urls')),
 )
